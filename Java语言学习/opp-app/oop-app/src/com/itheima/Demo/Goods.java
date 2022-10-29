5c467304f33a60565f56c2acdedc5b2fbedb8c88
YunXiaoYang003 <1919133601@qq.com>
2022-10-29 09:11:42 +0800

    2022.10.29

--- Java语言学习/opp-app/oop-app/src/com/itheima/Demo/Goods.java
+++
@@ -1,8 +0,0 @@
-package com.itheima.Demo;
-
-public class Goods {
-    int id;
-    String name;
-    double price;
-    int buyNumber;
-}
--- Java语言学习/opp-app/oop-app/src/com/itheima/createobject/Test2.java
+++
@@ -1,4 +0,0 @@
-package com.itheima.createobject;
-
-public class Test2 {
-}
