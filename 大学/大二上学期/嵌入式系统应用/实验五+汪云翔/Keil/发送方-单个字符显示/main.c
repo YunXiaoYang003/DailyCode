9406fb5dd668aaa5fbf26d8ac2ae21b1fb22fce2
YunXiaoYang003 <1919133601@qq.com>
2022-11-06 14:10:56 +0800

    2022.11.06

---
+++ 大学/大二上学期/嵌入式系统应用/实验五+汪云翔/Keil/发送方-单个字符显示/main.c
@@ -0,0 +1,23 @@
+      #include <reg52.h>
+      #include <uart.h>
+      #include <delay.h>
+      void DelayMs(unsigned char t);
+      //发送可以不用中断，接收一般用中断
+      void uart_init()
+      { SCON=0x50;   //串口方式1  
+        TMOD=0X20;  TH1=0x1D; TL1=0x1D;  TR1=1; //137.5BPS
+      }
+     main()
+     { //串口初始化
+       char name[]={"WangYunXiang"};
+       char i;
+       uart_init();
+       while(1)
+       {
+       SendStr(name); 
+       for(i=0;i<5;i++) //发送周期1s
+       DelayMs(250);
+       }  
+     }
+     
+     
